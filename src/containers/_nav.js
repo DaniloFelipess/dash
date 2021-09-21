5f17f3563f02873a79b7c400776e54d26f4dcc65
Danilo Santos <danilo.santos@nagem.com.br>
2021-09-21 18:12:30 -0300

    update de rota

--- src/containers/_nav.js
+++ src/containers/_nav.js
@@ -16,205 +16,40 @@ const _nav =  [
     _tag: 'CSidebarNavTitle',
     _children: ['Services']
   },
+  
+  
   {
     _tag: 'CSidebarNavDropdown',
     name: ' MicroServices',
-    route: '/base',
+    route: '/microservices',
     icon: 'cil-puzzle',
     _children: [
-      // {
-      //   _tag: 'CSidebarNavItem',
-      //   name: 'Breadcrumb',
-      //   to: '/base/breadcrumbs',
-      // },
-      // {
-      //   _tag: 'CSidebarNavItem',
-      //   name: 'Pagination',
-      //   to: '/base/paginations',
-      // },
+      
       {
         _tag: 'CSidebarNavItem',
         name: 'Fedex',
-        to: '/base/cards',
-      },
-      // {
-      //   _tag: 'CSidebarNavItem',
-      //   name: 'Carousel',
-      //   to: '/base/carousels',
-      // },
-      // {
-      //   _tag: 'CSidebarNavItem',
-      //   name: 'Collapse',
-      //   to: '/base/collapses',
-      // },
-      // {
-      //   _tag: 'CSidebarNavItem',
-      //   name: 'Forms',
-      //   to: '/base/forms',
-      // },
-      // {
-      //   _tag: 'CSidebarNavItem',
-      //   name: 'Jumbotron',
-      //   to: '/base/jumbotrons',
-      // },
-      // {
-      //   _tag: 'CSidebarNavItem',
-      //   name: 'List group',
-      //   to: '/base/list-groups',
-      // },
-      // {
-      //   _tag: 'CSidebarNavItem',
-      //   name: 'Navs',
-      //   to: '/base/navs',
-      // },
-      // {
-      //   _tag: 'CSidebarNavItem',
-      //   name: 'Navbars',
-      //   to: '/base/navbars',
-      // },
-      // {
-      //   _tag: 'CSidebarNavItem',
-      //   name: 'Pagination',
-      //   to: '/base/paginations',
-      // },
-      // {
-      //   _tag: 'CSidebarNavItem',
-      //   name: 'Popovers',
-      //   to: '/base/popovers',
-      // },
-      // {
-      //   _tag: 'CSidebarNavItem',
-      //   name: 'Progress',
-      //   to: '/base/progress-bar',
-      // },
-      // {
-      //   _tag: 'CSidebarNavItem',
-      //   name: 'Switches',
-      //   to: '/base/switches',
-      // },
-      // {
-      //   _tag: 'CSidebarNavItem',
-      //   name: 'Tables',
-      //   to: '/base/tables',
-      // },
-      // {
-      //   _tag: 'CSidebarNavItem',
-      //   name: 'Tabs',
-      //   to: '/base/tabs',
-      // },
-      // {
-      //   _tag: 'CSidebarNavItem',
-      //   name: 'Tooltips',
-      //   to: '/base/tooltips',
-      // },
+        to: '/microservices/cards',
+      },      
     ],
   },
-  // {
-  //   _tag: 'CSidebarNavDropdown',
-  //   name: 'Buttons',
-  //   route: '/buttons',
-  //   icon: 'cil-cursor',
-  //   _children: [
-  //     {
-  //       _tag: 'CSidebarNavItem',
-  //       name: 'Buttons',
-  //       to: '/buttons/buttons',
-  //     },
-  //     {
-  //       _tag: 'CSidebarNavItem',
-  //       name: 'Brand buttons',
-  //       to: '/buttons/brand-buttons',
-  //     },
-  //     {
-  //       _tag: 'CSidebarNavItem',
-  //       name: 'Buttons groups',
-  //       to: '/buttons/button-groups',
-  //     },
-  //     {
-  //       _tag: 'CSidebarNavItem',
-  //       name: 'Dropdowns',
-  //       to: '/buttons/button-dropdowns',
-  //     }
-  //   ],
-  // },
-  // {
-  //   _tag: 'CSidebarNavItem',
-  //   name: 'Charts',
-  //   to: '/charts',
-  //   icon: 'cil-chart-pie'
-  // },
-  // {
-  //   _tag: 'CSidebarNavDropdown',
-  //   name: 'Icons',
-  //   route: '/icons',
-  //   icon: 'cil-star',
-  //   _children: [
-  //     {
-  //       _tag: 'CSidebarNavItem',
-  //       name: 'CoreUI Free',
-  //       to: '/icons/coreui-icons',
-  //       badge: {
-  //         color: 'success',
-  //         text: 'NEW',
-  //       },
-  //     },
-  //     {
-  //       _tag: 'CSidebarNavItem',
-  //       name: 'CoreUI Flags',
-  //       to: '/icons/flags',
-  //     },
-  //     {
-  //       _tag: 'CSidebarNavItem',
-  //       name: 'CoreUI Brands',
-  //       to: '/icons/brands',
-  //     },
-  //   ],
-  // },
-  // {
-  //   _tag: 'CSidebarNavDropdown',
-  //   name: 'Notifications',
-  //   route: '/notifications',
-  //   icon: 'cil-bell',
-  //   _children: [
-  //     {
-  //       _tag: 'CSidebarNavItem',
-  //       name: 'Alerts',
-  //       to: '/notifications/alerts',
-  //     },
-  //     {
-  //       _tag: 'CSidebarNavItem',
-  //       name: 'Badges',
-  //       to: '/notifications/badges',
-  //     },
-  //     {
-  //       _tag: 'CSidebarNavItem',
-  //       name: 'Modal',
-  //       to: '/notifications/modals',
-  //     },
-  //     {
-  //       _tag: 'CSidebarNavItem',
-  //       name: 'Toaster',
-  //       to: '/notifications/toaster'
-  //     }
-  //   ]
-  // },
-  // {
-  //   _tag: 'CSidebarNavItem',
-  //   name: 'Widgets',
-  //   to: '/widgets',
-  //   icon: 'cil-calculator',
-  //   badge: {
-  //     color: 'info',
-  //     text: 'NEW',
-  //   },
-  // },
-  // {
-  //   _tag: 'CSidebarNavDivider'
-  // },
-  // {
-  //   _tag: 'CSidebarNavTitle',
-  //   _children: ['Extras'],
-  // },
+
+  {
+    _tag: 'CSidebarNavDropdown',
+    name: ' Grid',
+    route: '/grid',
+    icon: 'cil-puzzle',
+    _children: [
+      
+      {
+        _tag: 'CSidebarNavItem',
+        name: 'Gird Table',
+        to: '/grid/grid/Grid',
+      },      
+    ],
+  },
+
+
+
   {
     _tag: 'CSidebarNavDropdown',
     name: 'Pages',
@@ -242,60 +77,7 @@ const _nav =  [
         to: '/500',
       },
     ],
-  },
-  // {
-  //   _tag: 'CSidebarNavItem',
-  //   name: 'Disabled',
-  //   icon: 'cil-ban',
-  //   badge: {
-  //     color: 'secondary',
-  //     text: 'NEW',
-  //   },
-  //   addLinkClass: 'c-disabled',
-  //   'disabled': true
-  // },
-  // {
-  //   _tag: 'CSidebarNavDivider',
-  //   className: 'm-2'
-  // },
-  // {
-  //   _tag: 'CSidebarNavTitle',
-  //   _children: ['Labels']
-  // },
-  // {
-  //   _tag: 'CSidebarNavItem',
-  //   name: 'Label danger',
-  //   to: '',
-  //   icon: {
-  //     name: 'cil-star',
-  //     className: 'text-danger'
-  //   },
-  //   label: true
-  // },
-  // {
-  //   _tag: 'CSidebarNavItem',
-  //   name: 'Label info',
-  //   to: '',
-  //   icon: {
-  //     name: 'cil-star',
-  //     className: 'text-info'
-  //   },
-  //   label: true
-  // },
-  // {
-  //   _tag: 'CSidebarNavItem',
-  //   name: 'Label warning',
-  //   to: '',
-  //   icon: {
-  //     name: 'cil-star',
-  //     className: 'text-warning'
-  //   },
-  //   label: true
-  // },
-  // {
-  //   _tag: 'CSidebarNavDivider',
-  //   className: 'm-2'
-  // }
+  },  
 ]
 
 export default _nav
